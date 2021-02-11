7a577a09663b50910974ee53243d6c80a1435099
JaquesBoeno <jaquesvagnerjunior@gmail.com>
2021-02-11 19:09:47 -0300

    adicionado contador

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,33 @@
+import {useState} from 'react'
+
 function Home(){
   return(
-    <h1>Home</h1>
+    <>
+      <h1>Home</h1>
+      <Contador/>
+    </>
   );
+}
+
+function Contador(){
+
+  const [contador,setContador] = useState(1);
+
+  function adicionar(){
+    setContador(contador +1);
+  }
+
+  function remover(){
+    setContador(contador -1);
+  }
+
+  return(
+    <div>
+      <div>{contador}</div>
+      <button onClick={adicionar}>Adicionar</button>
+      <button onClick={remover}>remover</button>
+    </div>
+  )
 }
 
 export default Home;
